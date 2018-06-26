a50d78404d0edebc99ab95fb49ca1ae5049f80f9
Colin Henry <cmhenry@unm.edu>
2018-06-26 09:41:14 -0600

    Logit, ANOVA, Data Cleaning, Opening Data lessons started; mLogit, oLogit, and OLS edited

---
+++ Logit_Regression/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+# library(Kendall)
+# library(gmodels)
+
+gss <- read.csv(file.path(find.package("swirl"), "Courses","POLS581_in_R","Logit_Regression","gss2012_subset.csv"))
