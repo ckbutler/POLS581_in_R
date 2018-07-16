293e122232f21f4fc13f1f2013a60e47f4559ec4
nunnally0 <41022929+nunnally0@users.noreply.github.com>
2018-07-16 17:51:18 -0500

    Update initLesson.R

--- Data_Visualization/initLesson.R
+++ Data_Visualization/initLesson.R
@@ -3,4 +3,4 @@
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
 
-world <- read.dta(file.path(find.package("swirl"), "Courses","POLS581_in_R","Data_Visualization","world.dta"))
+world <- read_dta(file.path(find.package("swirl"), "Courses","POLS581_in_R","Data_Visualization","world.dta"))
